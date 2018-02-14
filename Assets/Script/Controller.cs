1df1df1a343116d57cffcee97c154897fcb834ad
Hiro <realsk8s.h@gmail.com>
2018-02-13 23:00:31 -0800

    Initial Commit
    Bounding box calculated

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+    public Button btn;
+    public List<GameObject> list = new List<GameObject>();
+    public Bounds bounds;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Button button = btn.GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void TaskOnClick ()
+    {
+        Debug.Log("You have clicked the button!");
+        
+        mergeIntoParent();
+        bounds = drawBoundingBox(list);
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(bounds.center, bounds.size);
+    }
+
+    private void mergeIntoParent()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (i == 0)
+                list.Add(GameObject.Find("Cube"));
+            else
+                list.Add(GameObject.Find("Cube (" + i + ")"));
+        }
+
+        GameObject gameObject = new GameObject();
+        //gameObject.AddComponent<MeshFilter>();
+        //gameObject.AddComponent<MeshRenderer>();
+
+        foreach(GameObject g in list)
+        {
+            g.transform.parent = gameObject.transform;
+        }
+    }
+
+    /// <summary>
+    /// This function finds the bounding box for all game objects in the List
+    /// passed into the parameter.
+    /// Referenced from Unity community and API.
+    /// https://answers.unity.com/questions/777855/bounds-finding-box.html
+    /// https://docs.unity3d.com/ScriptReference/Bounds.html
+    /// </summary>
+    /// <param name="l"></param>
+    /// <returns>bounds</returns>
+    private static Bounds drawBoundingBox(List<GameObject> l)
+    {
+        if (l.Count == 0)
+        {
+            return new Bounds(Vector3.zero, Vector3.one);
+        }
+
+        float minX = Mathf.Infinity;
+        float maxX = -Mathf.Infinity;
+        float minY = Mathf.Infinity;
+        float maxY = -Mathf.Infinity;
+        float minZ = Mathf.Infinity;
+        float maxZ = -Mathf.Infinity;
+
+        Vector3[] points = new Vector3[8];
+
+        foreach (GameObject go in l)
+        {
+            getBoundsPointsNoAlloc(go, points);
+            foreach (Vector3 v in points)
+            {
+                if (v.x < minX) minX = v.x;
+                if (v.x > maxX) maxX = v.x;
+                if (v.y < minY) minY = v.y;
+                if (v.y > maxY) maxY = v.y;
+                if (v.z < minZ) minZ = v.z;
+                if (v.z > maxZ) maxZ = v.z;
+            }
+        }
+
+        float sizeX = maxX - minX;
+        float sizeY = maxY - minY;
+        float sizeZ = maxZ - minZ;
+
+        Vector3 center = new Vector3(minX + sizeX / 2.0f, minY + sizeY / 2.0f, minZ + sizeZ / 2.0f);
+
+        return new Bounds(center, new Vector3(sizeX, sizeY, sizeZ));
+    }
+
+    /// <summary>
+    /// This function is support function of drawBoundingBox.
+    /// Referenced from Unity community.
+    /// https://answers.unity.com/questions/777855/bounds-finding-box.html
+    /// https://docs.unity3d.com/ScriptReference/Bounds.html
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="points"></param>
+    private static void getBoundsPointsNoAlloc(GameObject go, Vector3[] points)
+    {
+        if (points == null || points.Length < 8)
+        {
+            Debug.Log("Bad Array");
+            return;
+        }
+        MeshFilter mf = go.GetComponent<MeshFilter>();
+        if (mf == null)
+        {
+            Debug.Log("No MeshFilter on object");
+            for (int i = 0; i < points.Length; i++)
+                points[i] = go.transform.position;
+            return;
+        }
+
+        Transform tr = go.transform;
+
+        Vector3 v3Center = mf.mesh.bounds.center;
+        Vector3 v3ext = mf.mesh.bounds.extents;
+
+        points[0] = tr.TransformPoint(new Vector3(v3Center.x - v3ext.x, v3Center.y + v3ext.y, v3Center.z - v3ext.z));  // Front top left corner
+        points[1] = tr.TransformPoint(new Vector3(v3Center.x + v3ext.x, v3Center.y + v3ext.y, v3Center.z - v3ext.z));  // Front top right corner
+        points[2] = tr.TransformPoint(new Vector3(v3Center.x - v3ext.x, v3Center.y - v3ext.y, v3Center.z - v3ext.z));  // Front bottom left corner
+        points[3] = tr.TransformPoint(new Vector3(v3Center.x + v3ext.x, v3Center.y - v3ext.y, v3Center.z - v3ext.z));  // Front bottom right corner
+        points[4] = tr.TransformPoint(new Vector3(v3Center.x - v3ext.x, v3Center.y + v3ext.y, v3Center.z + v3ext.z));  // Back top left corner
+        points[5] = tr.TransformPoint(new Vector3(v3Center.x + v3ext.x, v3Center.y + v3ext.y, v3Center.z + v3ext.z));  // Back top right corner
+        points[6] = tr.TransformPoint(new Vector3(v3Center.x - v3ext.x, v3Center.y - v3ext.y, v3Center.z + v3ext.z));  // Back bottom left corner
+        points[7] = tr.TransformPoint(new Vector3(v3Center.x + v3ext.x, v3Center.y - v3ext.y, v3Center.z + v3ext.z));  // Back bottom right corner
+    }
+}
---
+++ Assets/Script/Mesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mesh : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        GameObject obj = GameObject.Find("Cube");
+        UnityEngine.Mesh mesh = obj.GetComponent<MeshFilter>().mesh;
+
+        GameObject newObj = new GameObject();
+        MeshFilter filter = newObj.AddComponent<MeshFilter>();
+        MeshRenderer renderer = newObj.AddComponent<MeshRenderer>();
+
+        UnityEngine.Mesh newMesh = new UnityEngine.Mesh();
+        newMesh.vertices = mesh.vertices;
+        newMesh.triangles = mesh.triangles;
+        filter.mesh = newMesh;
+        
+        renderer.material = new Material(Shader.Find("Transparent/Diffuse"));
+        renderer.material.color = Color.green;
+    }
+}
